da15214582d21d48b42f2bd8ae53a8770012aee8
Leon-Chun <s1235753@gmail.com>
2022-10-24 23:20:58 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
+
 
 
 //使用套件後產生的
 const app = express()
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' })) 
+app.set('view engine', 'handlebars')
 
 // user body get 
 app.use(express.urlencoded({ extended: true }))
 
 //router setting
 app.get('/', (req, res) => {
-  res.send('This is my test page for initialization(初始化) ')
+  res.render('index') //本來是 res.send 改成 res.render
 })
 
 app.listen(PORT, () => {
