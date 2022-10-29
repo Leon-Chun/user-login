b34864016333bf9eb6c7fc82bcbd96d847a4858c
Leon-Chun <s1235753@gmail.com>
2022-10-29 22:34:10 +0800

    feat: add mongoose and dotenv

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+
+
 
 //使用套件後產生的
 const app = express()
+
+// dotenv setting
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' })) 
 app.set('view engine', 'handlebars')
 
 // user body get 
 app.use(express.urlencoded({ extended: true }))
+
+//database setting 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { // 連線異常
+  console.log('mongodb error')
+})
+db.once('open', () => { // 連線成功
+  console.log('mongodb connected')
+})
 
 //router setting
 app.get('/', (req, res) => {
